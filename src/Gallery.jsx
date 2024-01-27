446e9a3f6e5dc312b79e398b369b24e326ccc7e1
Faigism <faigyi@code.edu.az>
2024-01-27 16:24:47 +0400

    image render

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,35 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+axios
+const url =
+  'https://api.unsplash.com/search/users?client_id=B0uCT9Sis4YnIY-90OQYg6mCZAvVaAhUhQO974ntYnQ&query=monkey'
+
 const Gallery = () => {
-  return <h2>Gallery</h2>
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url)
+      return result.data
+    },
+  })
+  // console.log(response)
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+  const results = response.data.results
+  // console.log(results)
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.photos?.map((item2) => item2.urls.regular)
+        return <img className="img" src={url} key={url} />
+      })}
+    </section>
+  )
 }
 export default Gallery
