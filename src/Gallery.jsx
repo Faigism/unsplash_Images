fc1fe6f480c6c3395683a14e1fe031991df5ee2a
Faigism <faigyi@code.edu.az>
2024-01-28 00:07:48 +0400

    setup searchTerm

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,19 +1,21 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { useGlobalContext } from './context'
 
 axios
 const url =
-  'https://api.unsplash.com/search/users?client_id=B0uCT9Sis4YnIY-90OQYg6mCZAvVaAhUhQO974ntYnQ&query=monkey'
+  'https://api.unsplash.com/search/photos?client_id=B0uCT9Sis4YnIY-90OQYg6mCZAvVaAhUhQO974ntYnQ'
 
 const Gallery = () => {
+  const { searchTerm } = useGlobalContext()
   const response = useQuery({
     queryKey: ['images'],
     queryFn: async () => {
-      const result = await axios.get(url)
+      const result = await axios.get(`${url}&query=${searchTerm}`)
       return result.data
     },
   })
-  // console.log(response)
+
   if (response.isLoading) {
     return (
       <section className="image-container">
@@ -21,13 +23,28 @@ const Gallery = () => {
       </section>
     )
   }
+
   const results = response.data.results
-  // console.log(results)
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    )
+  }
+
   return (
     <section className="image-container">
       {results.map((item) => {
-        const url = item?.photos?.map((item2) => item2.urls.regular)
-        return <img className="img" src={url} key={url} />
+        const url = item?.urls?.regular
+        return (
+          <img
+            className="img"
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+          />
+        )
       })}
     </section>
   )
